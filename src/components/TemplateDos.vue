eb658c6d3c01dc48fd474a55336d3699450f6b14
Vega <obed.vega@teradata.com>
2024-04-22 22:41:00 -0700

    test

---
+++ src/components/TemplateDos.vue
@@ -0,0 +1,12 @@
+
+<template>
+    <div>
+    <div class="w3-container w3-center">
+    <h1>¡Bienvenido!</h1>
+    <p>Esperando los datos de la boda...</p>
+    </div>
+  </div>
+</template>
+<script>
+</script>
+<style></style>
